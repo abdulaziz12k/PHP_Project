03e38b5b695c0878177f0aeecf5111f8cfc9b514
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-01 14:30:15 +0000

    Sep 1, 2024, 5:30 PM

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
-use Illuminate\support\Facades\Route;
+use Illuminate\Support\Facades\Route;
 
+Route::view('/', 'welcome');
 
-Route::get('/',function(){
-    return view('home');
-});
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
